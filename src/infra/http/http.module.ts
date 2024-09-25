5685865a0cb400ce87224156ecd51fcdfcc67137
Thales Santana <thales.c.santana@outlook.com>
2024-09-25 20:24:58 -0300

    fix archive name

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { NotificationsController } from './controllers/notifications..controller';
+import { NotificationsController } from './controllers/notifications.controller';
 import { SendNotification } from 'src/app/use-cases/send-notification';
 import { DatabaseModule } from '../database/database.module';
 import { CancelNotification } from '@app/use-cases/cancel-notification';
